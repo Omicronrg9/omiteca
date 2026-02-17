81c73f832db1a24a78aee5ad5f5dc6cfe51de63d
Omicronrg9 <Omicronrg9@users.noreply.github.com>
2026-02-17 11:50:12 +0100

    Centralizamos cositas, a esto es a lo que llaman "Refactor"?
    Ni idea la verdad. Lo importante es que si puedo poner todos los links en un solo archivo, mejor que mejor!

---
+++ buttons/links.js
@@ -0,0 +1,384 @@
+// Configuración centralizada de links, a ver qué tal funciona. Mejor esto que estar modificando docenas de archivos, ¿no?
+
+// Espero que sí. Aquí los 100 primeros arreglos. Del 100 al 200, en otro js. Para no sobrecargar.
+
+window.LINKS = {
+  A07: {
+    PDF: "https://www.dropbox.com/scl/fi/mcj0qi07dmcm41osvrz81/A7-The-Binding-of-Isaac-Tomes-Arreglo.pdf?rlkey=9bxharu5sg81e66an07p3w5a1&dl=0",
+    MP3: "https://drive.google.com/file/d/1Vn_vqB7Q1wYy8E8y2l3xGKJMlE8_8eza8/view?usp=sharing"
+  },
+  A08: {
+    PDF: "https://www.dropbox.com/scl/fi/g70nb2s1xw2gtk0xzndgb/A8-Repentant-Partitura_y_Partes.pdf?rlkey=jlyji4urbxzp89qfk7dc1yb3m&dl=0",
+    MP3: "https://www.dropbox.com/scl/fi/96ubdudz8ul2xqan7f9qw/Repentant.mp3?rlkey=855tdb9fbc8ubvbbelcfeds6j&dl=0"
+  },
+  A09: {
+    PDF: "https://www.dropbox.com/scl/fi/v617665muvdqe86hcczfy/A09-Tomes-II.zip?rlkey=z6ptmg79q7w0f5z7ozciopuvo&dl=0",
+    MP3: "https://www.dropbox.com/scl/fi/p7eovjc7s83yzg08ttdg6/Tomes-II.mp3?rlkey=gvvpb26qo96b3qgihlavo14me&dl=0"
+  },
+  A10: {
+    PDF: "https://www.dropbox.com/scl/fi/iup4ugjm9b201wc2li37k/A10-The-Binding-of-Isaac.pdf?rlkey=sfc4sk9ws8sqhj478n4wuuawm&dl=0",
+    MP3: "https://www.dropbox.com/scl/fi/oibsaohhaka9xb363m76f/The-Binding-of-Isaac.mp3?rlkey=hzfox2mjtmbn2f8dvgqfm6u97&dl=0"
+  },
+  A11: {
+    PDF: "11pdf",
+    MP3: "https://www.dropbox.com/scl/fi/1tymvk3xopk42kezpxhll/The-Calm-Variations-II.mp3?rlkey=jaa4zyuw42xkuwvnvycx3hstd&dl=0"
+  },
+  A12: {
+    PDF: "12pdf",
+    MP3: "https://www.dropbox.com/scl/fi/ey1v1lskz0o92v8tazp0z/Pixel-Dungeon-Main-Theme.mp3?rlkey=wl4uhvkimgsv9v9cxlys78m7v&dl=0"
+  },
+  A13: {
+    PDF: "13pdf",
+    MP3: "https://www.dropbox.com/scl/fi/ybcdhe7h1vlr0p34949l5/Latter-Days.mp3?rlkey=ji55z0ot21kk1ji3xc9u6522n&dl=0"
+  },
+  A14: {
+    PDF: "14pdf",
+    MP3: "https://www.dropbox.com/scl/fi/2lvt0sz36vrd26hssgozm/Dan-Dan-kokoro-hikareteku.mp3?rlkey=wxkxs1z50fes5ji2119xla08q&dl=0"
+  },
+  A15: {
+    PDF: "https://www.dropbox.com/scl/fo/w8rlltx2upgpmwvjn6c1v/AA_VOzkdftkHgvjjc2_f1uM?rlkey=pcm8qls2y1ze26zvar36qysy1&dl=0",
+    MP3: "https://www.dropbox.com/scl/fi/6hp9x32e2l3fdz1diexb5/St-ndchen.mp3?rlkey=90r23gkhuxnxzwmeeo3uqy869&dl=0"
+  },
+  A16: {
+    PDF: "16pdf",
+    MP3: "A16mp3"
+  },
+  A17: {
+    PDF: "17pdf",
+    MP3: "A17mp3"
+  },
+  A18: {
+    PDF: "18pdf",
+    MP3: "A18mp3"
+  },
+  A19: {
+    PDF: "19pdf",
+    MP3: "A19mp3"
+  },
+  A20: {
+    PDF: "20pdf",
+    MP3: "A20mp3"
+  },
+  A21: {
+    PDF: "21pdf",
+    MP3: "A21mp3"
+  },
+  A22: {
+    PDF: "22pdf",
+    MP3: "A22mp3"
+  },
+  A23: {
+    PDF: "23pdf",
+    MP3: "A23mp3"
+  },
+  A24: {
+    PDF: "24pdf",
+    MP3: "A24mp3"
+  },
+  A25: {
+    PDF: "25pdf",
+    MP3: "A25mp3"
+  },
+  A26: {
+    PDF: "26pdf",
+    MP3: "A26mp3"
+  },
+  A27: {
+    PDF: "27pdf",
+    MP3: "A27mp3"
+  },
+  A28: {
+    PDF: "28pdf",
+    MP3: "A28mp3"
+  },
+  A29: {
+    PDF: "29pdf",
+    MP3: "A29mp3"
+  },
+  A30: {
+    PDF: "30pdf",
+    MP3: "A30mp3"
+  },
+  A31: {
+    PDF: "31pdf",
+    MP3: "A31mp3"
+  },
+  A32: {
+    PDF: "32pdf",
+    MP3: "A32mp3"
+  },
+  A33: {
+    PDF: "33pdf",
+    MP3: "A33mp3"
+  },
+  A34: {
+    PDF: "34pdf",
+    MP3: "A34mp3"
+  },
+  A35: {
+    PDF: "35pdf",
+    MP3: "A35mp3"
+  },
+  A36: {
+    PDF: "36pdf",
+    MP3: "A36mp3"
+  },
+  A37: {
+    PDF: "37pdf",
+    MP3: "A37mp3"
+  },
+  A38: {
+    PDF: "38pdf",
+    MP3: "A38mp3"
+  },
+  A39: {
+    PDF: "39pdf",
+    MP3: "A39mp3"
+  },
+  A40: {
+    PDF: "40pdf",
+    MP3: "A40mp3"
+  },
+  A41: {
+    PDF: "41pdf",
+    MP3: "A41mp3"
+  },
+  A42: {
+    PDF: "42pdf",
+    MP3: "A42mp3"
+  },
+  A43: {
+    PDF: "43pdf",
+    MP3: "A43mp3"
+  },
+  A44: {
+    PDF: "44pdf",
+    MP3: "A44mp3"
+  },
+  A45: {
+    PDF: "45pdf",
+    MP3: "A45mp3"
+  },
+  A46: {
+    PDF: "46pdf",
+    MP3: "A46mp3"
+  },
+  A47: {
+    PDF: "47pdf",
+    MP3: "A47mp3"
+  },
+  A48: {
+    PDF: "48pdf",
+    MP3: "A48mp3"
+  },
+  A49: {
+    PDF: "49pdf",
+    MP3: "A49mp3"
+  },
+  A50: {
+    PDF: "50pdf",
+    MP3: "A50mp3"
+  },
+  A51: {
+    PDF: "51pdf",
+    MP3: "A51mp3"
+  },
+  A52: {
+    PDF: "52pdf",
+    MP3: "A52mp3"
+  },
+  A53: {
+    PDF: "53pdf",
+    MP3: "A53mp3"
+  },
+  A54: {
+    PDF: "54pdf",
+    MP3: "A54mp3"
+  },
+  A55: {
+    PDF: "55pdf",
+    MP3: "A55mp3"
+  },
+  A56: {
+    PDF: "56pdf",
+    MP3: "A56mp3"
+  },
+  A57: {
+    PDF: "57pdf",
+    MP3: "A57mp3"
+  },
+  A58: {
+    PDF: "58pdf",
+    MP3: "A58mp3"
+  },
+  A59: {
+    PDF: "59pdf",
+    MP3: "A59mp3"
+  },
+  A60: {
+    PDF: "60pdf",
+    MP3: "A60mp3"
+  },
+  A61: {
+    PDF: "61pdf",
+    MP3: "A61mp3"
+  },
+  A62: {
+    PDF: "62pdf",
+    MP3: "A62mp3"
+  },
+  A63: {
+    PDF: "63pdf",
+    MP3: "A63mp3"
+  },
+  A64: {
+    PDF: "64pdf",
+    MP3: "A64mp3"
+  },
+  A65: {
+    PDF: "65pdf",
+    MP3: "A65mp3"
+  },
+  A66: {
+    PDF: "66pdf",
+    MP3: "A66mp3"
+  },
+  A67: {
+    PDF: "67pdf",
+    MP3: "A67mp3"
+  },
+  A68: {
+    PDF: "68pdf",
+    MP3: "A68mp3"
+  },
+  A69: {
+    PDF: "69pdf",
+    MP3: "A69mp3"
+  },
+  A70: {
+    PDF: "70pdf",
+    MP3: "A70mp3"
+  },
+  A71: {
+    PDF: "71pdf",
+    MP3: "A71mp3"
+  },
+  A72: {
+    PDF: "72pdf",
+    MP3: "A72mp3"
+  },
+  A73: {
+    PDF: "73pdf",
+    MP3: "A73mp3"
+  },
+  A74: {
+    PDF: "74pdf",
+    MP3: "A74mp3"
+  },
+  A75: {
+    PDF: "75pdf",
+    MP3: "A75mp3"
+  },
+  A76: {
+    PDF: "76pdf",
+    MP3: "A76mp3"
+  },
+  A77: {
+    PDF: "77pdf",
+    MP3: "A77mp3"
+  },
+  A78: {
+    PDF: "78pdf",
+    MP3: "A78mp3"
+  },
+  A79: {
+    PDF: "79pdf",
+    MP3: "A79mp3"
+  },
+  A80: {
+    PDF: "80pdf",
+    MP3: "A80mp3"
+  },
+  A81: {
+    PDF: "81pdf",
+    MP3: "A81mp3"
+  },
+  A82: {
+    PDF: "82pdf",
+    MP3: "A82mp3"
+  },
+  A83: {
+    PDF: "83pdf",
+    MP3: "A83mp3"
+  },
+  A84: {
+    PDF: "84pdf",
+    MP3: "A84mp3"
+  },
+  A85: {
+    PDF: "85pdf",
+    MP3: "A85mp3"
+  },
+  A86: {
+    PDF: "86pdf",
+    MP3: "A86mp3"
+  },
+  A87: {
+    PDF: "87pdf",
+    MP3: "A87mp3"
+  },
+  A88: {
+    PDF: "88pdf",
+    MP3: "A88mp3"
+  },
+  A89: {
+    PDF: "89pdf",
+    MP3: "A89mp3"
+  },
+  A90: {
+    PDF: "90pdf",
+    MP3: "A90mp3"
+  },
+  A91: {
+    PDF: "91pdf",
+    MP3: "A91mp3"
+  },
+  A92: {
+    PDF: "92pdf",
+    MP3: "A92mp3"
+  },
+  A93: {
+    PDF: "93pdf",
+    MP3: "A93mp3"
+  },
+  A94: {
+    PDF: "94pdf",
+    MP3: "A94mp3"
+  },
+  A95: {
+    PDF: "95pdf",
+    MP3: "A95mp3"
+  },
+  A96: {
+    PDF: "96pdf",
+    MP3: "A96mp3"
+  },
+  A97: {
+    PDF: "97pdf",
+    MP3: "A97mp3"
+  },
+  A98: {
+    PDF: "98pdf",
+    MP3: "A98mp3"
+  },
+  A99: {
+    PDF: "99pdf",
+    MP3: "A99mp3"
+  },
+  A100: {
+    PDF: "100pdf",
+    MP3: "A100mp3"
+  }
+  // Y los que sigan... Probablemente se irán a links2.js y mantendrán el nombre de variable. Así no cargo tantas cosas.
+};
+
